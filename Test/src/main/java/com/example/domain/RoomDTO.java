6544a2bfcc3e6322432afcb169a40f9b1c7bdb52
it <tlgud200@naver.com>
2024-04-09 16:39:01 +0900

    Room 매퍼 테스트 중

---
+++ Test/src/main/java/com/example/domain/RoomDTO.java
@@ -0,0 +1,15 @@
+package com.example.domain;
+
+import lombok.Data;
+
+@Data
+public class RoomDTO {
+
+	private int RO_NUM;
+	private String AC_ID;
+	private String RO_NAME;
+	private int RO_BASIC;
+	private int RO_MAX;
+	private String RO_INFO;
+	
+}
---
+++ Test/src/main/java/com/example/mapper/RoomMapper.java
@@ -0,0 +1,13 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import com.example.domain.RoomDTO;
+
+public interface RoomMapper {
+	
+	public List<RoomDTO> getList();
+		
+
+	
+}
